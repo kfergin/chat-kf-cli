bfd708ff3a1ce765623f5ad16456222441f804ec
Kaleb Fergin <kaleb@kalebfergin.com>
2023-11-28 18:55:18 -0700

    Wire up to openai model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-process.stdin.setEncoding('utf8');
-process.stderr.setEncoding('utf8');
+import path from 'path';
+
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+// the default `path` is `path.resolve(process.cwd(), '.env')`
+// and I want to call this command from anywhere
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 async function getMessage() {
   const [messageArg] = process.argv.slice(2);
@@ -29,14 +35,44 @@ async function main() {
   process.stdin.setEncoding('utf8');
   process.stderr.setEncoding('utf8');
 
-  const [errorMessage, message] = await getMessage();
+  const [errorMessage, content] = await getMessage();
 
   if (errorMessage) {
     process.stderr.write(errorMessage + '\n');
     process.exit(1);
   }
 
-  process.stdout.write(message + '\n');
+  process.stderr.write('\n' + String.fromCodePoint(0x1f916) + '...\n\n');
+
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content }],
+      model: 'gpt-4-1106-preview',
+      stream: true,
+      // temperature: 0,
+      // max_tokens: maxTokensResponse,
+    });
+
+    for await (const chunk of completion) {
+      const content = chunk.choices[0].delta.content;
+      // e.g.
+      // `choices: [ { index: 0, delta: { content: '.' }, finish_reason: null } ]`
+      // `choices: [ { index: 0, delta: {}, finish_reason: 'stop' } ]`
+      if (content) {
+        process.stdout.write(content);
+      }
+    }
+    process.stdout.write('\n\n');
+  } catch (error) {
+    process.stderr.write('An error occurred while processing the request.\n');
+    process.stderr.write(error?.toString() ?? '(No error message)');
+    process.stderr.write('\n');
+    process.exit(1);
+  }
 }
 
 main();
