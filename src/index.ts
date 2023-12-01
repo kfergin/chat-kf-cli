b946a22f64669181f6ccded0c27ccd2fd097c829
Kaleb Fergin <kaleb@kalebfergin.com>
2023-12-01 12:57:09 -0700

    Support options flags

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,42 @@ import askGpt from './ask-gpt';
 // and I want to call this command from anywhere
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
-async function getMessage() {
-  const [messageArg] = process.argv.slice(2);
+const HELP_MESSAGE = `USAGE:
+
+\`\`\`shell
+$ kf-chat-cli [option flags] "What is the meaning of life?"
+\`\`\`
+
+Stdin can also be used to pass a message to gpt
+
+\`\`\`shell
+$ kf-chat-cli [option flags] <<'EOF'
+heredoc>What does this do?
+heredoc>\`\`\`javascript
+heredoc>  console.log('42');
+heredoc>\`\`\`
+heredoc>EOF
+$
+# OR
+$ cat some-file.ts | kf-chat-cli [option flags]
+\`\`\`
+
+`;
+
+async function getContentAndOptions(): Promise<[string, string[]]> {
+  const [messageArg, options] = process.argv
+    .slice(2)
+    .reduce<[string, string[]]>(
+      (arr, arg) => {
+        if (/^-/.test(arg)) {
+          arr[1].push(arg);
+        } else {
+          arr[0] = arg;
+        }
+        return arr;
+      },
+      ['', []],
+    );
 
   const message = await new Promise<string>((resolve) => {
     const timeoutId = setTimeout(() => resolve(''), 100);
@@ -33,14 +67,19 @@ async function getMessage() {
     process.exit(1);
   }
 
-  return message;
+  return [message, options];
 }
 
 async function main() {
   process.stdin.setEncoding('utf8');
   process.stderr.setEncoding('utf8');
 
-  const content = await getMessage();
+  const [content, options] = await getContentAndOptions();
+
+  if (options.some((opt) => /-h|--help/.test(opt))) {
+    process.stdout.write(HELP_MESSAGE);
+    process.exit(0);
+  }
 
   askGpt(content as string);
 }
