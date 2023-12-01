5b52d963575d3e264189eecc9e1b39933361751d
Kaleb Fergin <kaleb@kalebfergin.com>
2023-12-01 11:54:43 -0700

    Move gpt request to separate function

---
+++ src/ask-gpt.ts
@@ -0,0 +1,35 @@
+import OpenAI from 'openai';
+
+export default async function askGpt(content: string) {
+  process.stderr.write('\n' + String.fromCodePoint(0x1f916) + '...\n\n');
+
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content }],
+      model: 'gpt-4-1106-preview',
+      stream: true,
+      // temperature: 0,
+      // max_tokens: maxTokensResponse,
+    });
+
+    for await (const chunk of completion) {
+      const content = chunk.choices[0].delta.content;
+      // e.g.
+      // `choices: [ { index: 0, delta: { content: '.' }, finish_reason: null } ]`
+      // `choices: [ { index: 0, delta: {}, finish_reason: 'stop' } ]`
+      if (content) {
+        process.stdout.write(content);
+      }
+    }
+    process.stdout.write('\n\n');
+  } catch (error) {
+    process.stderr.write('An error occurred while processing the request.\n');
+    process.stderr.write(error?.toString() ?? '(No error message)');
+    process.stderr.write('\n');
+    process.exit(1);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 
-import OpenAI from 'openai';
 import dotenv from 'dotenv';
+
+import askGpt from './ask-gpt';
 
 // the default `path` is `path.resolve(process.cwd(), '.env')`
 // and I want to call this command from anywhere
@@ -42,37 +43,7 @@ async function main() {
     process.exit(1);
   }
 
-  process.stderr.write('\n' + String.fromCodePoint(0x1f916) + '...\n\n');
-
-  try {
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content }],
-      model: 'gpt-4-1106-preview',
-      stream: true,
-      // temperature: 0,
-      // max_tokens: maxTokensResponse,
-    });
-
-    for await (const chunk of completion) {
-      const content = chunk.choices[0].delta.content;
-      // e.g.
-      // `choices: [ { index: 0, delta: { content: '.' }, finish_reason: null } ]`
-      // `choices: [ { index: 0, delta: {}, finish_reason: 'stop' } ]`
-      if (content) {
-        process.stdout.write(content);
-      }
-    }
-    process.stdout.write('\n\n');
-  } catch (error) {
-    process.stderr.write('An error occurred while processing the request.\n');
-    process.stderr.write(error?.toString() ?? '(No error message)');
-    process.stderr.write('\n');
-    process.exit(1);
-  }
+  askGpt(content as string);
 }
 
 main();
