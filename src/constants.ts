13839aec489c52d622bd786d681e57423376cd08
Kaleb Fergin <kaleb@kalebfergin.com>
2024-02-10 13:39:06 -0700

    Move help message to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,53 @@ export const dataDir = path.join(__dirname, '../data/');
 export const conversationsDir = path.join(dataDir, './conversations/');
 
 export const isTerminal = process.stdout.isTTY;
+
+export const HELP_MESSAGE = `Usage: kf-chat-cli [options] [message]
+
+Arguments:
+  message       Text to be processed by the application. If not provided,
+                the application will wait to receive input from stdin.
+
+Options:
+  -c, --continue-conversation=<id>
+                Continues a conversation using the specified conversation ID.
+                If no ID is provided, the application will use the current
+                conversation state.
+
+  -h, --help    Displays this help message and exits.
+
+  -l, --list-conversations
+                Lists all available conversations.
+
+  -v, --view-conversation=<id>
+                Views a particular conversation given its ID. If no ID is
+                provided, the application will use the current conversation
+                state.
+
+Examples:
+  kf-chat-cli -c "Hello world"
+                Sends "Hello world" message and continues with the current
+                conversation.
+
+  kf-chat-cli --list-conversations
+                Lists all conversations without sending a message.
+
+  kf-chat-cli -v=12345
+                Views the conversation with ID 12345.
+
+  echo "Hello again" | kf-chat-cli
+                Pipes "Hello again" message into the application and waits
+                for further instructions.
+
+  kf-chat-cli <<'EOF'
+  heredoc> Hello, what does \`ls\` do?
+  heredoc> EOF
+                Uses a heredoc to send a message. Useful to not not worry
+                about escaping lines.
+
+Note:
+  If a conversation ID is not specified with the '-c' or '-v' options and
+  the current conversation state exists, the application will default to
+  the current conversation.
+
+`;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 import dotenv from 'dotenv';
 
-import { conversationsDir, dataDir } from './constants';
+import { HELP_MESSAGE, conversationsDir, dataDir } from './constants';
 
 import getContentAndOptions from './get-content-and-options';
 import askGpt from './ask-gpt';
@@ -13,56 +13,6 @@ import viewConversation from './view-conversation';
 // the default `path` is `path.resolve(process.cwd(), '.env')`
 // and I want to call this command from anywhere
 dotenv.config({ path: path.join(__dirname, '../.env') });
-
-const HELP_MESSAGE = `Usage: kf-chat-cli [options] [message]
-
-Arguments:
-  message       Text to be processed by the application. If not provided,
-                the application will wait to receive input from stdin.
-
-Options:
-  -c, --continue-conversation=<id>
-                Continues a conversation using the specified conversation ID.
-                If no ID is provided, the application will use the current
-                conversation state.
-
-  -h, --help    Displays this help message and exits.
-
-  -l, --list-conversations
-                Lists all available conversations.
-
-  -v, --view-conversation=<id>
-                Views a particular conversation given its ID. If no ID is
-                provided, the application will use the current conversation
-                state.
-
-Examples:
-  kf-chat-cli -c "Hello world"
-                Sends "Hello world" message and continues with the current
-                conversation.
-
-  kf-chat-cli --list-conversations
-                Lists all conversations without sending a message.
-
-  kf-chat-cli -v=12345
-                Views the conversation with ID 12345.
-
-  echo "Hello again" | kf-chat-cli
-                Pipes "Hello again" message into the application and waits
-                for further instructions.
-
-  kf-chat-cli <<'EOF'
-  heredoc> Hello, what does \`ls\` do?
-  heredoc> EOF
-                Uses a heredoc to send a message. Useful to not not worry
-                about escaping lines.
-
-Note:
-  If a conversation ID is not specified with the '-c' or '-v' options and
-  the current conversation state exists, the application will default to
-  the current conversation.
-
-`;
 
 async function main() {
   process.stdin.setEncoding('utf8');
