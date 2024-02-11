68834efd0b7a18f7aef6bf26d0e2f0e9fb9ce649
Kaleb Fergin <kaleb@kalebfergin.com>
2024-02-11 12:04:27 -0700

    Add confirmation step when deleting

--- src/delete-conversation.ts
+++ src/delete-conversation.ts
@@ -1,27 +1,65 @@
+import readline from 'readline';
+
 import { isTerminal } from './constants';
 import {
   deleteConversation as deleteConversationFile,
+  getConversation,
   readState,
   writeState,
 } from './utils';
+
+function confirm(message: string) {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    // Ask for confirmation
+    rl.question(message, (answer) => {
+      if (answer.toLowerCase() === 'yes') {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+      rl.close();
+    });
+  });
+}
 
 export default async function deleteConversation(
   conversationId: string | null,
 ) {
   if (!conversationId) {
-    process.stderr.write('Please specify a conversation to delete\n');
+    process.stderr.write('Please specify a conversation to delete\n\n');
     process.exit(1);
   }
 
+  let firstMessage;
   try {
-    await deleteConversationFile(conversationId);
-  } catch (error: unknown) {
+    firstMessage = await getConversation(conversationId).then(([messages]) =>
+      messages[0].content.trim(),
+    );
+  } catch (error) {
     if ((error as NodeJS.ErrnoException)?.code === 'ENOENT') {
-      process.stderr.write(`Invalid conversation id: "${conversationId}\n`);
+      process.stderr.write(`Invalid conversation id: "${conversationId}"\n\n`);
       process.exit(1);
     } else {
       throw error;
     }
+  }
+
+  const shouldContinue = await confirm(
+    `
+Conversation Id: ${conversationId}
+First Message: ${firstMessage}
+
+Are you sure you want delete this conversation? (yes/no) `,
+  );
+
+  if (!shouldContinue) {
+    process.stdout.write('Canceling...\n\n');
+    process.exit(0);
   }
 
   const { currentConversation } = await readState();
@@ -29,10 +67,11 @@ export default async function deleteConversation(
     await writeState({ currentConversation: null });
   }
 
+  await deleteConversationFile(conversationId);
+
   if (isTerminal) {
     process.stdout.write('\n');
   }
 
-  process.stdout.write(`Id: ${conversationId}\n`);
-  process.stdout.write(`Conversation deleted\n\n`);
+  process.stdout.write(`Deleted.\n\n`);
 }
