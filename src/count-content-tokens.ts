f1d4dbdeabbc62df28620754ac75172cc5558bb4
Kaleb Fergin <kaleb@kalebfergin.com>
2025-03-01 16:14:50 -0700

    Upgrade tiktoken

--- src/count-content-tokens.ts
+++ src/count-content-tokens.ts
@@ -18,7 +18,6 @@ export default function countContentTokens(content: string, modelName: string) {
     process.exit(1);
   }
 
-  // @ts-expect-error tiktoken's types are not up to date
   const encoding = encoding_for_model(modelName);
   const tokenCount = encoding.encode(content).length;
   encoding.free();
