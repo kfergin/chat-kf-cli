72c40c2decac1dd2aac5b0bbb67544f4305c7709
Kaleb Fergin <kaleb@kalebfergin.com>
2025-03-01 17:00:58 -0700

    Set model using subset of name

--- src/set-model.ts
+++ src/set-model.ts
@@ -2,16 +2,20 @@ import { AVAILABLE_MODELS } from './constants';
 import { isValidModelName, patchState } from './utils';
 
 export default async function setModel(modelName: string) {
-  if (!isValidModelName(modelName)) {
+  const modelToSet = isValidModelName(modelName)
+    ? modelName
+    : AVAILABLE_MODELS.find((model) => model.includes(modelName));
+
+  if (!modelToSet) {
     process.stderr.write(
-      `Invalid model name. Available options: ${AVAILABLE_MODELS.join(', ')}\n`,
+      `No matching model. Available options: ${AVAILABLE_MODELS.join(', ')}\n`,
     );
     process.exit(1);
   }
 
   try {
-    await patchState({ selectedModel: modelName });
-    process.stdout.write(`Model set to "${modelName}"\n`);
+    await patchState({ selectedModel: modelToSet });
+    process.stdout.write(`Model set to "${modelToSet}"\n`);
   } catch (error) {
     console.log(error);
     let errorMessage = 'Unknown error';
